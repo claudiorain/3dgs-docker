1c4088944da8af6700e2bf7ef56bbd3ebb2a5623
Claudio Pioggia <claudio.pioggia@gmail.com>
2025-02-10 22:58:05 +0100

    Added web-viewer docker config

--- gaussian-splatting/api.py
+++ gaussian-splatting/api.py
@@ -5,7 +5,11 @@ import subprocess
 app = FastAPI()
 
 class ConvertRequest(BaseModel):
-    target_dir: str  # Deve corrispondere al payload che invii
+    input_dir: str  # Deve corrispondere al payload che invii
+
+class TrainRequest(BaseModel):
+    input_dir: str  # Deve corrispondere al payload che invii
+    output_dir: str  # Deve corrispondere al payload che invii
 
 
 @app.get("/")
@@ -14,33 +18,38 @@ def read_root():
 
 @app.post("/convert")
 def run_convert(request: ConvertRequest):
-    command = f"python /workspace/gaussian-splatting/convert.py -s {request.target_dir}"
+    command = f"python /workspace/gaussian-splatting/convert.py -s {request.input_dir}"
     
-    print("Target directory:" + request.target_dir)
+    print("Target directory:" + request.input_dir)
     process = subprocess.Popen(
         command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
     )
 
-    stdout_log = []
-    stderr_log = []
+    stdout, stderr = process.communicate()  # Legge tutto l'output
 
-    # Legge l'output in tempo reale
-    for line in iter(process.stdout.readline, ''):
-        stdout_log.append(line.strip())
-        print(f"STDOUT: {line.strip()}")  # Stampa nel log del container
+    print(f"STDOUT:\n{stdout}")  # Mostra il log dell'output
+    print(f"STDERR:\n{stderr}")
 
-    for line in iter(process.stderr.readline, ''):
-        stderr_log.append(line.strip())
-        print(f"STDERR: {line.strip()}")  # Stampa nel log del container
-
-    process.stdout.close()
-    process.stderr.close()
-    process.wait()  # Aspetta la fine del processo
-
-    return {"stdout": stdout_log, "stderr": stderr_log}
+    return {"stdout": stdout, "stderr": stderr}
 
 @app.post("/train")
-def run_train(input_path: str):
-    command = f"python /workspace/gaussian-splatting/train.py -s {input_path} --densify_grad_threshold 0.001 --densification_interval 500 --densify_until_iter 7000 --test_iterations -1"
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
-    return {"stdout": result.stdout, "stderr": result.stderr}
+def run_train(request: TrainRequest):
+    print("Input directory:" + request.input_dir)
+    print("Input directory:" + request.output_dir)
+
+    command = f"python /workspace/gaussian-splatting/train.py -s {request.input_dir} -m {request.output_dir} --densify_grad_threshold 0.001 --densification_interval 500 --densify_until_iter 7000 --test_iterations -1"
+    
+    process = subprocess.Popen(
+        command, 
+        shell=True, 
+        stdout=subprocess.PIPE, 
+        stderr=subprocess.PIPE, 
+        text=True  # Decodifica direttamente in stringhe
+    )
+
+    stdout, stderr = process.communicate()  # Legge tutto l'output
+
+    print(f"STDOUT:\n{stdout}")  # Mostra il log dell'output
+    print(f"STDERR:\n{stderr}")
+
+    return {"stdout": stdout, "stderr": stderr}
